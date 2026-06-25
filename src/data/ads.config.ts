d824f00ac6f76437733243ca62d5a71f85fa5cd0
sshheerraallii <sshheerraallii@gmail.com>
2026-06-25 18:42:10 +0500

    Add real Adsterra ad codes

---
+++ src/data/ads.config.ts
@@ -0,0 +1,70 @@
+export const ADS_ENABLED = true;
+export const adCodes: Record<string, string> = {
+  'banner-468x60': `<script>
+  atOptions = {
+    'key' : '8137cf37a134780072a462c2a459d19b',
+    'format' : 'iframe',
+    'height' : 60,
+    'width' : 468,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/8137cf37a134780072a462c2a459d19b/invoke.js"></script>`,
+  'banner-160x300': `<script>
+  atOptions = {
+    'key' : '1dc44a28077530fc7e95066e551f50d4',
+    'format' : 'iframe',
+    'height' : 300,
+    'width' : 160,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/1dc44a28077530fc7e95066e551f50d4/invoke.js"></script>`,
+  'banner-320x50': `<script>
+  atOptions = {
+    'key' : '505cee68bbe87bbe99f733f25b4919a3',
+    'format' : 'iframe',
+    'height' : 50,
+    'width' : 320,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/505cee68bbe87bbe99f733f25b4919a3/invoke.js"></script>`,
+  'banner-728x90': `<script>
+  atOptions = {
+    'key' : '7f923ae1a73a8238380524a3b2b15a22',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/7f923ae1a73a8238380524a3b2b15a22/invoke.js"></script>`,
+  'banner-160x600': `<script>
+  atOptions = {
+    'key' : 'fec16b7a966a72476dd49b21ec3b62b7',
+    'format' : 'iframe',
+    'height' : 600,
+    'width' : 160,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/fec16b7a966a72476dd49b21ec3b62b7/invoke.js"></script>`,
+  'banner-300x250': `<script>
+  atOptions = {
+    'key' : 'e28a4f0c3e74644064f63a55e0888d54',
+    'format' : 'iframe',
+    'height' : 250,
+    'width' : 300,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/e28a4f0c3e74644064f63a55e0888d54/invoke.js"></script>`,
+  native: `<script async="async" data-cfasync="false" src="https://pl29895284.effectivecpmnetwork.com/02ed1c99a7307f16c2a59a03db4c0b45/invoke.js"></script>
+<div id="container-02ed1c99a7307f16c2a59a03db4c0b45"></div>`,
+};
+export const popunderCode = `<script src="https://pl29895282.effectivecpmnetwork.com/42/49/df/4249dfabc6e3a1e69bbff1ba2ceb2c67.js"></script>`;
+export const socialBarCode = `<script src="https://pl29895285.effectivecpmnetwork.com/8c/f1/3d/8cf13d6c2bd45e880f3b04522be22494.js"></script>`;
+export const POPUNDER_FIRST_DELAY_MS = 30_000;
+export const POPUNDER_REPEAT_DELAY_MS = 300_000;
+export const SOCIAL_BAR_DELAY_MS = 30_000;
